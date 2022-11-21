e13b7685a019fc2faaf103914e485055e18afb06
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-21 12:22:44 +0530

    initial commit

---
+++ src/ProgramMenu.java
@@ -0,0 +1,21 @@
+public class ProgramMenu {
+    public static void main(String[] args) {
+        System.out.println("MENU");
+        System.out.println("1.Largest of 3 numbers");
+        System.out.println("2.smallest of 3 numbers");
+        System.out.println("3.Prime number or not");
+        System.out.println("4.Even or Not");
+        System.out.println("5.Reverse of a number");
+
+        int flag=1;
+        while(flag == 1){
+
+
+
+
+
+
+        }
+
+    }
+}
